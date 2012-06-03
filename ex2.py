95c5e1572d7d13c0b263165b6ba72903828770f6
lan <lanyg.com@gmail.com>
2012-06-03 23:04:19 +0800

    Comments And Pound Characters

---
+++ ex2.py
@@ -0,0 +1,13 @@
+# -- coding: utf-8--
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print "This will run."
+
+# 可以加入中文注释，但必须头部加上字符解释 coding: utf-8 或 utf8 或 UTF-8 或 UTF8 。
